3c24f958cd3964e79aae2f42dbd384ab8d1ea5dd
Ian Forrest <ianforrest11@gmail.com>
2019-09-20 11:00:42 -0400

    push scafter tutorial, finished pt 2

--- flask_practice/flaskblog.py
+++ flask_practice/flaskblog.py
@@ -1,15 +1,31 @@
-from flask import Flask
-
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/home')
-def home():
-    return "<h1>homepage</h1>"
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
 
-@app.route('/about')
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
 def about():
-    return "<h1>aboutpage</h1>"
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
