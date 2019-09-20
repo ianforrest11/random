309ea3ce512212cb02731ea210501a4144f5cbd2
Ian Forrest <ianforrest11@gmail.com>
2019-09-20 15:19:29 -0400

    push progress to git, 1821 of vid 3

--- flask_practice/flaskblog.py
+++ flask_practice/flaskblog.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+# protects against modifying cookies, etc
+app.config['SECRET_KEY'] = '2436fc61bf4c195d7473043d5a93a63d'
 
 posts = [
     {
@@ -27,6 +31,15 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
